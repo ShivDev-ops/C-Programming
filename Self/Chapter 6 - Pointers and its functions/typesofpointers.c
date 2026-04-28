f8052b2130810bd796acf0b10b7e292467c1a4a4
Kaustubh Shukla <kaustubhshukla9586@gmail.com>
2026-04-28 13:02:54 +0530

    pointer

--- Self/Chapter 6 - Pointers and its functions/typesofpointers.c
+++ Self/Chapter 6 - Pointers and its functions/typesofpointers.c
@@ -1,9 +1,12 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 /*
 1 - Null Pointer - A pointer when it is not pointing to any variable.
 2 - Wild Pointer - Any pointer which is declared but not initialized. 
 3 - Void Pointer - Generic pointer which can point to any data type.
+4 - Dangling Pointer - 
+5 - Constant Pointer - 
 
 */
 
@@ -26,6 +29,17 @@ int main() {
     // void *ptr = &a;  
     // void *ptr1 = &b;
     // void *ptr2 = &c;
+
+    // Constant Pointer
+
+    // Dangling Pointer
+    int *ptr = (int*)malloc(4); // 4 is the value of this (number * sizeof(datatype)) stores the amount of variables we are initializing
+    scanf("%d",ptr); // not using &ptr because ptr already has the address of that variable
+    printf("%d",*ptr);
+    free(ptr);
+    // printf("%d",*ptr); // This will give you a error. This is dangling pointer when you dont have a fixed memory location for that specific pointer.
+    ptr = NULL; //
+    printf("%d",ptr); // This will also not work
 
     
     // printf("%d\n",*(int*)ptr); // To de reference the pointer we use this type cast to print the output
